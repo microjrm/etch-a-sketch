2b45df7714430c6b994cdc538de0439805473b5c
Jonathan Motyka <motyk1jr@gmail.com>
2017-08-26 00:19:48 -0400

    Add files via upload
    Created a working sketchpad with well rounded functions.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 August 2, 2017:
   The hardest part is getting started right?  My goal for the session was to get a solid foundation down in HTML/CSS/JS for creating a grid using divs.  I spent the last few days reading up on the different ways that effect can be achieved.  I opted to go with creating a table of divs inside of a larger div ('container').  The good news is that the tiny bit of code that I wrote is bug free.  The bad news is that I cannot seem to get the grid to visualize itself in the browser using CSS. I'm sure it's something that has a simple solution, but I didn't have much time to work with it today so I will leave that up to the next session.   
+
+
+August 25, 2017:
+  I took a long break from working on the sketchpad due to some more important events in my life.  But now I am back and determined to finish this project.  I went back to the drawing board and decided to create divs using a loop rather than using a table method.  It works great and I was even able to add some buttons to adjust the size and color of the grid using jQuery.  The last thing I would like to do before moving on is create a 10% fade effect.  I would also like to do a little messing around with CSS just to see if I could make the page look a little more lively.  
+
+  
--- function.js
+++ function.js
@@ -1,18 +1,73 @@
-$(document).ready(function() {
 
-  $("#container").append('<table class = "table"></table>');
+//Create grid funtion
+function createGrid(x) {
+  for (var rows = 0; rows < x; rows++){
+    for (var cols = 0; cols < x; cols++){
+      $("#container").append("<div class = 'grid' ></div>");
+    };
+  };
+  $(".grid").height(800/x);
+  $(".grid").width(800/x);
+};
 
-  function createGrid(x) {
 
-  for (var i = 0; i < x; i++) {
-    $('.table').append('<tr></tr>');
-  }
+//Clear grid function
+function clearGrid() {
+  $(".grid").remove();
+};
 
-  for (var j = 0; j < x; j++) {
-    $('.table').append('<td></td>');
-  }
-
-  createGrid(16);
+//New grid with x number of squares function
+function newGrid() {
+  var j = prompt("How many squares would you like your grid to be?");
+  clearGrid();
+  createGrid(j);
 }
 
+
+
+//Creates a 16x16 grid when the page loads and allows for clickable buttons
+$(document).ready(function(){
+  createGrid(16);
+
+ $(".grid").mouseover(function(){
+   $(this).css("background-color", "black");
+  });
+
+//Select new sizeable BLACK grid with each mouseover
+ $(".blackGrid").click(function(){
+   newGrid();
+
+   $(".grid").mouseover(function(){
+     $(this).css("background-color", "black");
+    });
+  });
+
+
+//Select new sizeable RANDOM grid with each mouseover
+   $(".randomGrid").click(function(){
+     newGrid();
+
+     $(".grid").on('mouseover', function(){
+       var random = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+       $(this).css("background-color", random);
+     });
+  });
+
+//Select new sizable FADE grid with each mouseover
+ $(".fadeGrid").click(function(){
+  newGrid();
+
+   $(".grid").on('mouseover', function(){
+    $(this).fadeIn('slow', '.1');
+    });
+  });
+
+//Clear the grid with button click and return it to 16x16 black
+  $(".clearGrid").click(function(){
+    clearGrid();
+    createGrid(16);
+    $(".grid").mouseover(function(){
+      $(this).css("background-color", "black");
+     });
+  });
 });
